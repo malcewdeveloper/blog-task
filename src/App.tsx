d2da560901ffbc721081aae6390e5f315e53b50d
Maksim <malcewdeveloper@yandex.ru>
2024-03-29 16:06:49 +0300

    feat: adds a routes handler

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Blog, Post } from "./pages";
+import { store } from "./redux/store";
+import { Provider } from "react-redux";
 import 'normalize.css';
+
 
 export default function App() {
     return (
-        <div>Hello world!</div>
+        <main>
+            <Provider store={ store }>
+                <BrowserRouter basename="/">
+                    <Routes>
+                        <Route path="/" element={ <Blog /> } />
+                        <Route path=":id" element={ <Post /> } />
+                    </Routes>
+                </BrowserRouter>
+            </Provider>
+        </main>
     )
 }
